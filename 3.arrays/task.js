29470e50c08bf8fef5d89e8a57c2a0469ac7b22c
Maria <marija89@sibmail.com>
2023-02-28 11:34:57 +0700

    Homework3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 
   return result; // boolean
+  
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+console.log(compareArrays([8, 5, 3], [8, 5, 3]));
 
-  // Ваш код
+function advancedFilter(arr) {
+
+  let resultArr = arr.filter(Number => Number > 0).filter(Number => Number % 3 === 0).map(Number => Number*10);
+
 
   return resultArr; // array
 }
+console.log(advancedFilter([-1, 6, -9, 3]));
