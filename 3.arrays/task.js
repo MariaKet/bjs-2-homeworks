8ee19367d60559d8d27dad122dcc35da000a1baa
Maria <marija89@sibmail.com>
2023-03-01 11:42:46 +0700

    3Homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
-
   return result; // boolean
-  
 }
 
 console.log(compareArrays([8, 5, 3], [8, 5, 3]));
 
 function advancedFilter(arr) {
-
-  let resultArr = arr.filter(Number => Number > 0).filter(Number => Number % 3 === 0).map(Number => Number*10);
-
-
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
   return resultArr; // array
 }
+
 console.log(advancedFilter([-1, 6, -9, 3]));
