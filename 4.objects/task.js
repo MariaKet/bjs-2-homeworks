9046f56d7507c6d1795e367aa2eb55a1c90b7a05
Maria <marija89@sibmail.com>
2023-03-21 15:45:35 +0700

    Homework4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
-function Student(name, gender, age) {
-    // Ваш код
 
+
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
+
+console.log(student1);
+console.log(student2);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];  // Первая оценка добавляется в массив
+  } else {
+    this.marks.push(mark); // Вторая и последующая оценки пушатся
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks = this.marks.concat(marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
